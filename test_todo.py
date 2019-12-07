b52f0bb468a6fdf25c6b81bae18b70bbbd0c46fa
Wonho ha <haho013039@gmail.com>
2019-12-07 18:49:22 +0900

    Add Repository

---
+++ test_todo.py
@@ -0,0 +1,45 @@
+from todo_repository import InMemoryRepository
+from todo import Todo
+
+
+def test_todo():
+    # Given
+    todo1 = Todo("Las's Todo", "Today")
+    todo2 = Todo("Nam's Todo", "Next Day")
+    repository = InMemoryRepository()
+
+    # When
+    repository.add_todo(todo1)
+    repository.add_todo(todo2)
+
+    # Then
+    assert todo1 in repository.get_by_date("Today")
+
+
+def test_todo_search_fail():
+    # Given
+    todo1 = Todo("Las's Todo", "Today")
+    todo2 = Todo("Nam's Todo", "Next Day")
+    repository = InMemoryRepository()
+
+    # When
+    repository.add_todo(todo1)
+    repository.add_todo(todo2)
+
+    # Then
+    assert repository.get_by_date("Yesterday") == []
+
+
+def test_today_have_multiple_todo():
+    # Given
+    todo1 = Todo("Las's Todo", "Today")
+    todo2 = Todo("Nam's Todo", "Today")
+    repository = InMemoryRepository()
+
+    # When
+    repository.add_todo(todo1)
+    repository.add_todo(todo2)
+
+    # Then
+    assert todo1 in repository.get_by_date("Today")
+    assert todo2 in repository.get_by_date("Today")
