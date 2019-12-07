c4c576512150cf71803dd6041c1ad22d9ddc88a6
goto <45956673+Goto-Namj@users.noreply.github.com>
2019-12-07 19:58:47 +0900

    remove_todo

--- test_todo.py
+++ test_todo.py
@@ -43,3 +43,19 @@ def test_today_have_multiple_todo():
     # Then
     assert todo1 in repository.get_by_date("Today")
     assert todo2 in repository.get_by_date("Today")
+
+
+def test_todo_remove():
+    # Given
+    todo1 = Todo("Las's Todo", "Today")
+    todo2 = Todo("Nam's Todo", "Today")
+    repository = InMemoryRepository()
+    repository.add_todo(todo1)
+    repository.add_todo(todo2)
+
+    # When
+    repository.remove_todo(todo2)
+
+    # Then
+    assert todo1 in repository.get_by_date("Today")
+    assert todo2 not in repository.get_by_date("Today")
